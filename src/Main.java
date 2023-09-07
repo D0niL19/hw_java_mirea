81f38ca174ca70d0dd213524e91b43cb7d34198a
D0niL <kdi190104@mail.ru>
2023-09-07 17:55:22 +0300

    second ex is done

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    static void Ex3()
+    {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++)
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = scanner.nextInt();
+            sum += arr[i];
+        }
+        System.out.println(sum + " " + (float)sum / n);
+    }
+    static void Ex4()
+    {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
         {
             arr[i] = scanner.nextInt();
         }
-        for (int i = 0; i < n; i++)
+        int mx = 0, mn = 100000;
+        int k = 0;
+        while (k < n)
         {
-            System.out.println(arr[i] + " ");
-        }
+            if (arr[k] > mx) mx = arr[k];
 
+            if (arr[k] < mn) mn = arr[k];
+            k++;
+        }
+        System.out.println("max = " + mx + "\nmin = " + mn);
+    }
+    public static void main(String[] args) {
+        Ex4();
     }
 }
